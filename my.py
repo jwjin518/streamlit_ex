7abe620bda1e7e41313c269e49f74e461f8e4fbd
jwjin518 <58908821+jwjin518@users.noreply.github.com>
2025-08-20 14:40:43 +0900

    Update my.py

--- my.py
+++ my.py
@@ -5,8 +5,7 @@ import time
 st.title("반응속도 테스트")
 if st.button("클릭하면 시작합니다"):
     st.image("https://static.streamlit.io/examples/cat.jpg", caption="귀여운 고양이", use_container_width=True)
-    while True:
-        rate(60)
+ 
 
 
     
