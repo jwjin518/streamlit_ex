3812b27a073a4581f27a30c06b5a966b49f8d1e6
jwjin518 <58908821+jwjin518@users.noreply.github.com>
2025-08-20 14:30:05 +0900

    Update my.py

--- my.py
+++ my.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import numpy as np
+
+st.title("반응속도 테스트")
+if st.button("클릭하면 시작합니다"):
+    st.image("https://static.streamlit.io/examples/cat.jpg", caption="귀여운 고양이", use_container_width=True)
+    while
 
 
-if st.button("클릭하세요"):
+    
     st.write("버튼이 클릭되었습니다!")
-st.code("""
-import streamlit as st
-st.title('Hello World')
-""", language="python")
+
+
+
+
+
+
+
+
+
+
+
